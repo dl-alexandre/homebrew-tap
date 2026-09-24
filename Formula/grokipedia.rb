e351e89bf389d82e53661ce687e65c574d52f1e6
dl-alexandre <alley.nightly_0b@icloud.com>
2026-09-24 09:23:22 -0700

    chore: update Grokipedia package metadata

--- Formula/grokipedia.rb
+++ Formula/grokipedia.rb
@@ -4,21 +4,21 @@
 class Grokipedia < Formula
   desc "Grokipedia CLI - command-line interface for the Grokipedia API"
   homepage "https://github.com/dl-alexandre/Grokipedia-CLI"
-  version "v0.1.1"
+  version "v0.1.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/dl-alexandre/Grokipedia-CLI/releases/download/v0.1.1/grokipedia-darwin-amd64.tar.gz"
-      sha256 "e381e0cc18c2f06497bc1893d509e8305bd4ddbec7b194c35fa354e354b92e92"
+      url "https://github.com/dl-alexandre/Grokipedia-CLI/releases/download/v0.1.2/grokipedia-darwin-amd64.tar.gz"
+      sha256 "e4a62cdbb0380bcadf7be81f784a47d23021e99f3debf59e7706adc0b30cf18a"
 
       define_method(:install) do
         bin.install "grokipedia"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/dl-alexandre/Grokipedia-CLI/releases/download/v0.1.1/grokipedia-darwin-arm64.tar.gz"
-      sha256 "a5270ab2f28d605787ee51db670c9c0e0d6d36987f8ac9659729bf8b7c13d46c"
+      url "https://github.com/dl-alexandre/Grokipedia-CLI/releases/download/v0.1.2/grokipedia-darwin-arm64.tar.gz"
+      sha256 "91fdf10750e97ee6c68512e0339de60d292a2046e24e013fffe876f30bffd562"
 
       define_method(:install) do
         bin.install "grokipedia"
@@ -28,15 +28,15 @@ class Grokipedia < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dl-alexandre/Grokipedia-CLI/releases/download/v0.1.1/grokipedia-linux-amd64.tar.gz"
-      sha256 "e0a5b578a05c0e4281b0f26c713d49bcc0a6879fbac327824746a999382d62cc"
+      url "https://github.com/dl-alexandre/Grokipedia-CLI/releases/download/v0.1.2/grokipedia-linux-amd64.tar.gz"
+      sha256 "46095ec9e524cf054ba38151d37066e845aa5b627c633f18b6866b1a7f694efc"
       define_method(:install) do
         bin.install "grokipedia"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dl-alexandre/Grokipedia-CLI/releases/download/v0.1.1/grokipedia-linux-arm64.tar.gz"
-      sha256 "0f6c65ff5ec5346369e6e3563e4a9eb50406bad4dcd03de427c1acfa84304db6"
+      url "https://github.com/dl-alexandre/Grokipedia-CLI/releases/download/v0.1.2/grokipedia-linux-arm64.tar.gz"
+      sha256 "9c6b69a7672da71b74bc5fbf636d93e8755807c130ed2659b484d8e8b763dbdc"
       define_method(:install) do
         bin.install "grokipedia"
       end
